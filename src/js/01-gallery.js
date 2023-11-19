f22a12d1175906f7474d600a9f43ba3813834330
mareckkkk <m.walus94@gmail.com>
2023-11-19 22:06:36 +0100

    change src file

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,25 +1,5 @@
-import SimpleLightbox from "simplelightbox";
-import "simplelightbox/dist/simple-lightbox.min.css";
-
 // Add imports above this line
-import { galleryItems } from "./gallery-items";
+import { galleryItems } from './gallery-items';
 // Change code below this line
 
-const gallery = document.querySelector(".gallery");
-
-const markup = galleryItems
-	.map(
-		({ preview, description, original }) =>
-			`<a class="gallery__item" href="${original}">
-            <img class="gallery__image" src="${preview}" alt="${description}" />
-        </a>`
-	)
-	.join("");
-
-gallery.insertAdjacentHTML("beforeend", markup);
-
-const lightbox = new SimpleLightbox(".gallery a", {
-	captionsData: "alt",
-	captionDelay: 250,
-	captionPosition: "bottom",
-});
+console.log(galleryItems);
